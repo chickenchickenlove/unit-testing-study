1e08890fb690f8b1360d758776fd5bdcba37fee5
chickenchickenlove <ojt90902@naver.com>
2023-03-30 22:10:47 +0900

    hello

--- src/main/java/chapter11/MyCalculator.java
+++ src/main/java/chapter11/MyCalculator.java
@@ -3,27 +3,4 @@ package chapter11;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-
-public class MyCalculator {
-
-    public int calculate(int n1, int n2) {
-        return n1 + n2;
-    }
-
-    @Test
-    void test() {
-        // given
-        MyCalculator myCalculator = new MyCalculator();
-        int n1 = 1;
-        int n2 = 2;
-        int expected = n1 + n2; // 도메인 지식 유출
-
-        // when
-        int calculate = myCalculator.calculate(n1, n2);
-
-        // then
-
-        assertThat(calculate).isEqualTo(expected);
-    }
-}
+import static org.assertj.core.api.Assertions.
